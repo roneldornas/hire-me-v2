a1a39b9dbcb938a2763175d437ae6dc6fa389863
Rônel Dornas <roneldornas@gmail.com>
2018-04-14 10:38:36 -0300

    navigationoptions

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AuthScreen extends Component {
+  static navigationOptions = ({}) => {
+    return {}
+  }
+
   render(){
     return(
       <View>
